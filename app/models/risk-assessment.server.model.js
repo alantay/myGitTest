a47c8f6a794386e13f7c85b834cc67b1d07a26eb
Alan Tay <alantay@Alans-MacBook-Air.local>
2015-05-24 10:12:02 +0800

    testing

--- app/models/risk-assessment.server.model.js
+++ app/models/risk-assessment.server.model.js
@@ -9,6 +9,108 @@ var mongoose = require('mongoose'),
 /**
  * Risk assessment Schema
  */
+var RiskControl = new Schema({
+    description:{
+        type:String,
+        default: '',
+        required: 'Please fill description of risk control',
+        trim: true
+    },
+    severity:{
+        type:Number,
+        default: '',
+        required: 'Please fill severity',
+        trim: true
+    },
+    likelihood:{
+        type:Number,
+        default: '',
+        required: 'Please fill likelihood',
+        trim: true
+    }
+});
+
+var PossibleDanger = new Schema({
+    description:{
+        type:String,
+        default: '',
+        required: 'Please fill description of possible danger',
+        trim: true
+    },
+    risk_control:{
+        description:{
+            type:String,
+            default: '',
+            required: 'Please fill description of risk control',
+            trim: true
+        },
+        severity:{
+            type:Number,
+            default: '',
+            required: 'Please fill severity',
+            trim: true
+        },
+        likelihood:{
+            type:Number,
+            default: '',
+            required: 'Please fill likelihood',
+            trim: true
+        }
+
+    },
+
+    implementation :[{
+        type: Schema.ObjectId,
+        ref: 'User'
+    }],
+
+    due_date : {
+        type:Date,
+        required:'Please fill in last review date'
+    },
+    remarks:{
+        type:String,
+        default: '',
+        required: 'Please fill in remarks',
+        trim: true
+    }
+});
+
+var Hazard = new Schema({
+    hazard_type:{
+        type:String,
+        required : 'Please fill in hazard type'
+    },
+    possible_danger:[PossibleDanger]
+});
+
+var RiskAssessmentDetail = new Schema({
+    last_review:{
+        type:Date,
+        required:'Please fill in last review date'
+    },
+    next_review:{
+        type:Date,
+        required:'Please fill in last review date'
+    },
+    ra_leader: [{
+        type: Schema.ObjectId,
+        ref: 'User'
+    }],
+    approved:[{
+        type: Schema.ObjectId,
+        ref: 'User'
+    }],
+    work_activity:{
+        type: String,
+        default: '',
+        required: 'Please fill work activity name',
+        trim: true
+    },
+    hazard:[Hazard]
+});
+
+
 var RiskAssessmentSchema = new Schema({
 	name: {
 		type: String,
@@ -16,6 +118,39 @@ var RiskAssessmentSchema = new Schema({
 		required: 'Please fill Risk assessment name',
 		trim: true
 	},
+
+    ssn :{
+        type: String,
+        default: '',
+        required: 'Error, no ssn',
+        trim: true
+    },
+    department : {
+        type: String,
+        default: '',
+        required: 'Please fill department name',
+        trim: true
+    },
+    process : {
+        type: String,
+        default: '',
+        required: 'Please fill process name',
+        trim: true
+    },
+    location :{
+        type: String,
+        default: '',
+        required: 'Please fill location name',
+        trim: true
+    },
+
+    original_assessment_date :{
+        type: Date,
+        default: '',
+        required: 'Please fill in the date'
+    },
+    versions :[RiskAssessmentDetail],
+
 	created: {
 		type: Date,
 		default: Date.now
